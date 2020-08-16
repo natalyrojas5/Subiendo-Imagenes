aa49abd7885434ba188394685db16052fd6d83d6
Nataly Rojas <nataly@ludik.pe>
2020-08-16 17:27:42 -0500

    FEAT: Producción

---
+++ docs/precache-manifest.54971eb3953121e3c749f82aa75c40da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab9c794aaec79f111c400bde3238c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260637e207a6ca400253",
+    "url": "/static/css/main.eda0ba94.chunk.css"
+  },
+  {
+    "revision": "b7f5d1a08b09dd325da6",
+    "url": "/static/js/2.e19d98dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e19d98dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260637e207a6ca400253",
+    "url": "/static/js/main.03d8954b.chunk.js"
+  },
+  {
+    "revision": "3d84cdbef791b13945f3",
+    "url": "/static/js/runtime-main.5c98c4fa.js"
+  }
+]);
